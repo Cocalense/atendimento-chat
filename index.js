c6504d33ea7e009b17c09595b6df5fb1a75b52cb
Anderson Colossi <cocalense@gmail.com>
2020-04-01 05:16:44 -0300

    iniciando js

---
+++ index.js
@@ -0,0 +1,9 @@
+<script>
+function openul() {
+  document.getElementById("botaoEnviar").style.display = "block";
+}
+
+function closeul() {
+  document.getElementById("botaoEnviar").style.display = "none";
+}
+</script>
