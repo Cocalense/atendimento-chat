2b1c5e36899b584622b1d7980cbe43d0589232e5
Anderson Colossi <cocalense@gmail.com>
2020-04-25 03:11:31 -0300

    testando partes de JS
    !--  ANALISANDO E COMPARANDO ESTA SECÇÃO HTML. ...
                    <div class="containerLeft">
                    <p class="atendente">Atendente diz:</p>
                    <div class="caixaLeft">
                        <p class="mensagemTextoL"> blablabla</p>
                    </div>
                -->

--- novoChat/chat.js
+++ novoChat/chat.js
@@ -1,2 +1,14 @@
-var h = window.document.getElementById('titulo')
-titulo.innerHTML = 'Atendimento on-line'
+function carregar() {
+    var hdr = window.document.getElementById('hdr')
+    hdr.innerHTML ='mudando o texto'
+}
+
+
+function envMsg(){
+    var mensagem = window.document.getElementById('digitando').value;
+    if(mensagem.trim() !== ''){
+        window.document.getElementById('digitando').value = '';
+        var container = document.createElement("div");
+        container.classList.add("")
+    }
+}
